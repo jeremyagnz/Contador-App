7dbd3d1ab2851184547569f9ac88925e7f2a46c6
jeremyagnz <jeremyagnz@gmail.com>
2022-10-28 16:02:25 -0400

    Adding Modules

--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
 import { Personaje } from '../interfaces/dbz.interface';
 
-
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.css'],
 })
 export class MainPageComponent {
-
   personajes: Personaje[] = [
     {
       nombre: 'Goku',
@@ -24,6 +22,7 @@ export class MainPageComponent {
     nombre: 'Kakaroto',
     poder: 1110,
   };
-  
-
+  agregarNuevoPersonaje( argumento:Personaje ){
+    this.personajes.push(argumento);
+  }
 }
